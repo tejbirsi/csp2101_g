a3f6b7e53f5bec689fabd18b428a3739dffa76a1
tejbirsi <63444669+tejbirsi@users.noreply.github.com>
2020-04-10 16:37:14 +0800

    Update guessage.sh
    comments

--- portfolio1/guessage.sh
+++ portfolio1/guessage.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+
+# this generates a random number between 20 and 70 and assigns it to age
 age=$(( RANDOM %(70-20  +1) +20))
+
+#infite loop
 for (( ; ; ))
 do 
   read -p"Guess the age between 20-70: " user_guess
+  
+  #if user enters anything except integers it prompts them to enter the required input
   if ! [[ "$user_guess" =~ ^[0-9]+(\.[0-9]+)?$ ]]
   then
     echo "Please enter a number between 20 and 70"
     continue
   fi
+  
+  #if user guesses the age then it tells them the answer is correct aand vice versa
   if [ "${user_guess%%.*}" -eq "$age" ]
   then 
     echo "Your guess is correct"
